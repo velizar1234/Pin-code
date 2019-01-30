29c4c4d5f4cb670f2132c992f7aa4bba268d44b4
velizar1234 <velizar5513@gmail.com>
2019-01-30 21:05:08 +0000

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PINCODE = 0000;
+            int failCounter = 0;
+            do
+            {
+                Console.WriteLine("Enter your pin code");
+                string strValue = Console.ReadLine();
+                int theValue = Convert.ToInt32(strValue);
+
+                if (theValue == PINCODE)
+                {
+                    Console.WriteLine("Thank you very much");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    failCounter = failCounter + 1;
+                    Console.WriteLine("Wrong ping, please try again");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            } while (failCounter > 3);
+        }
+    }
+}
